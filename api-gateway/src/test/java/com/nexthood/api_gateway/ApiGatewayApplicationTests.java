d52f5cffcfb6b8b4f93490c0773dc3c2fe3cf78d
Abinesh <abinesh.r.2019.cse@ritchennai.edu.in>
2025-07-28 21:51:45 +0530

    commit setup

---
+++ api-gateway/src/test/java/com/nexthood/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexthood.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
