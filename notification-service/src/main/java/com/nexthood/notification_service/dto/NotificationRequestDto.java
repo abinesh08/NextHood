33b10ddc42d4a567c22d770b2c6b7ae4830e4c2e
Abinesh <abinesh.r.2019.cse@ritchennai.edu.in>
2025-08-15 21:52:56 +0530

    commit notification complete test

--- notification-service/src/main/java/com/nexthood/notification_service/dto/NotificationRequestDto.java
+++ notification-service/src/main/java/com/nexthood/notification_service/dto/NotificationRequestDto.java
@@ -4,11 +4,15 @@ import com.nexthood.notification_service.model.NotificationType;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationRequestDto {
     @NotNull
     private Long userId;
--- notification-service/src/main/java/com/nexthood/notification_service/dto/NotificationResponseDto.java
+++ notification-service/src/main/java/com/nexthood/notification_service/dto/NotificationResponseDto.java
@@ -1,13 +1,17 @@
 package com.nexthood.notification_service.dto;
 
 import com.nexthood.notification_service.model.NotificationType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationResponseDto {
     private Long id;
     private Long userId;
